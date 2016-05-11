300fc64a37e1605f3fe4365f093370652c28b3c0
mumboz <git@mumboz.dk>
2016-05-11 22:57:10 +0200

    Missing semi-colons and some white-spacing cleaned up

--- browser_action_clicked.js
+++ browser_action_clicked.js
@@ -1,14 +1,14 @@
 var uri = document.documentURI;
-var newURL = "http://sullygnome.com"
+var newURL = "http://sullygnome.com";
 
 
 if (uri.includes("www.twitch.tv/directory/game/"))
- {
- 	var gameName = uri.substring(uri.lastIndexOf("/") + 1, uri.length);
- 	newURL += "/Game/" + gameName;
- }
- else if (uri.includes("https://www.twitch.tv/"))
- {
+{
+	var gameName = uri.substring(uri.lastIndexOf("/") + 1, uri.length);
+	newURL += "/Game/" + gameName;
+}
+else if (uri.includes("https://www.twitch.tv/"))
+{
  	var channelName = uri.substring(uri.lastIndexOf("/") + 1, uri.length);
 	if (channelName.length > 0)
 	{
