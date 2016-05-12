1b01b7261178332c0f1c8dc17c8ed639a21bbdd7
mumboz <git@mumboz.dk>
2016-05-12 22:33:17 +0200

    Added support for game names on directory pages

--- content_script.js
+++ content_script.js
@@ -59,20 +59,34 @@ function addLinksToDocument()
 		}
 	}
 	
-	// Directory pages - page header
-	var jqTitleElement = $(".directory_header").find(".title");
-	if (jqTitleElement.length > 0)
+	// Directory pages
+	if (document.documentURI.includes("www.twitch.tv/directory"))
 	{
-		var gameName = jqTitleElement.get(0).childNodes[1].data;
-		jqTitleElement.append(createLink("http://sullygnome.com/Game/" + gameName));
+		// All games - page header
+		if (document.documentURI.includes("www.twitch.tv/directory/game"))
+		{
+			var jqTitleElement = $(".directory_header").find(".title");
+			if (jqTitleElement.length > 0)
+			{
+				var gameName = jqTitleElement.get(0).childNodes[1].data;
+				jqTitleElement.append(createLink("http://sullygnome.com/Game/" + gameName));
+			}
+		}
+
+		// Channel names
+		$("p.title").find("[data-channel-link]").each( function()
+		{
+			var chanUrl = $(this).attr("href");
+			$(this).before(createLink("http://sullygnome.com/Channel" + chanUrl));	
+		});
+		
+		// Game names
+		$("a.game-item").each( function()
+		{
+			var gameName = $(this).attr("title");
+			$(this).after(createLink("http://sullygnome.com/Game/" + gameName));
+		});
 	}
-	
-	// Directory pages - channel names
-	$("p.title").find("[data-channel-link]").each( function()
-	{
-		var chanUrl = $(this).attr("href");
-		$(this).before(createLink("http://sullygnome.com/Channel" + chanUrl));	
-	});
 }
 
 // Check for tag before adding links to prevent adding them multiple times
